cae9ae3fb63e584f7c765632b83f3efc4af28ae5
Tom Ganemo <tom.ganemo1987@gmail.com>
2021-11-16 10:59:25 +0100

    Fixed basic Question display Getting data, semi - fixed waiting for input from server

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -6,12 +6,14 @@
  */
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 import java.util.*;
 import javax.swing.*;
 
-public class ClientTest {
+public class ClientTest implements ActionListener {
 
     JFrame jf = new JFrame();
     JPanel questionPanel = new JPanel();
@@ -20,6 +22,24 @@ public class ClientTest {
     JButton alt2 = new JButton();
     JButton alt3 = new JButton();
     JButton alt4 = new JButton();
+    PrintWriter out;
+    ObjectInputStream in;
+
+    String theQuestion;
+    String alt1a;
+    String alt2a;
+    String alt3a;
+    String alt4a;
+
+
+
+    public String getToSendBackToServer() {
+        return toSendBackToServer;
+    }
+    public void setToSendBackToServer(String toSendBackToServer) {
+        this.toSendBackToServer = toSendBackToServer;
+    }
+    String toSendBackToServer = "This feedback";
 
 
 
@@ -32,20 +52,15 @@ public class ClientTest {
 
         try {
             Socket socket = new Socket(hostName, portNumber);
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+            in = new ObjectInputStream(socket.getInputStream());
 
             Object questionPackFromServer = in.readObject();
 
             System.out.println(questionPackFromServer.toString());
 
+            // objectInformationToStrings(questionPackFromServer);
             //TODO Bryt ut informationen från objectet till strängar för att populera spelplan
-            String theQuestion;
-            String alt1a;
-            String alt2a;
-            String alt3a;
-            String alt4a;
-
 
             jf.add(questionPanel);
             jf.setVisible(true);
@@ -60,15 +75,20 @@ public class ClientTest {
             questionPanel.add(question);
 
             alt1.setText("alternative 1");
+            alt1.addActionListener(this);
             questionPanel.add(alt1);
 
+
             alt2.setText("alternative 2");
+            alt2.addActionListener(this);
             questionPanel.add(alt2);
 
             alt3.setText("alternative 3");
+            alt3.addActionListener(this);
             questionPanel.add(alt3);
 
             alt4.setText("alternative 4");
+            alt4.addActionListener(this);
             questionPanel.add(alt4);
 
             jf.setLocationRelativeTo(null);
@@ -79,7 +99,54 @@ public class ClientTest {
             e.printStackTrace();
             e.getMessage();
         }
+
+
+
+
+
     }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource()== alt1){
+            System.out.println("Wrong answere!");
+
+        }
+
+        if(e.getSource()== alt2){
+            System.out.println("Wrong answere!");
+
+        }
+        if(e.getSource()== alt3){
+            System.out.println("Wrong answere!");
+
+        }
+        if(e.getSource()== alt4){
+            System.out.println("Correct Answere");
+            alt4.setBackground(Color.GREEN);
+            returnToServer();
+            // TODO kanske bara returna en Integer 1 eller 0 beroende på poäng.
+        }
+
+    }
+
+    public void returnToServer(){
+        setToSendBackToServer("Correct Answere 1");
+        out.write(getToSendBackToServer());
+    }
+
+    public void objectInformationToStrings(Object object){
+        String fullString = object.toString();
+        //fullString.split(",");
+
+
+    }
+
+
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ClientTest ct = new ClientTest();
     }

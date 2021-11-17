92bd4373b4451e6f836c050402b6b39da281e40e
EmilWikell <91671600+EmilWikell@users.noreply.github.com>
2021-11-17 12:42:31 +0100

    small fix

--- src/DispatchClasses/CategoryClass.java
+++ src/DispatchClasses/CategoryClass.java
@@ -3,8 +3,9 @@ package DispatchClasses;
 import java.io.Serializable;
 
 public class CategoryClass implements Serializable {
-    String categories;
+    String categories = "";
+
     public void addToList(String string){
-        categories += "," + string; // make sure to only use first four elements in split
+        categories += string + ","; // make sure to only use first four elements in split
     }
 }

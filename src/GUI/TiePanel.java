9021976acfd510c9151c786ef89f0a6f15d2de3e
Ali Moteirek <mtairek20@gmail.com>
2021-11-22 02:10:48 +0100

    Changed background and foreground color in tiepanel class

--- src/GUI/TiePanel.java
+++ src/GUI/TiePanel.java
@@ -12,6 +12,11 @@ public class TiePanel extends JPanel {
         this.setLayout(new GridLayout(1, 1));
 
         condition.setText("Game Tied");
+        condition.setHorizontalAlignment(SwingConstants.CENTER);
+        condition.setFont(new Font(Font.DIALOG_INPUT,Font.BOLD,30));
+        condition.setBackground(new Color (103, 51, 150));
+        condition.setOpaque(true);
+        condition.setForeground(Color.WHITE);
         this.add(condition);
     }
 }

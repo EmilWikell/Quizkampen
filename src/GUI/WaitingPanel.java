36ba22a9645e04c9d0aad88911e36d7d191c9d2f
Ali Moteirek <mtairek20@gmail.com>
2021-11-22 02:15:36 +0100

    Added a gif icon for waitingpanel

--- src/GUI/WaitingPanel.java
+++ src/GUI/WaitingPanel.java
@@ -6,13 +6,15 @@ import java.awt.*;
 public class WaitingPanel extends JPanel {
     public WaitingPanel(){
 
-        JLabel waiting = new JLabel();
-
+        //JLabel waiting = new JLabel();
+        ImageIcon icon = new ImageIcon("src/loading.gif") ;
+        Image image = icon.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT) ;
+        ImageIcon icon1 = new ImageIcon(image) ;
+        JLabel iconLabel = new JLabel(icon1);
         setPreferredSize(new Dimension(400, 700));
         this.setLayout(new GridLayout(1, 1));
 
-        waiting.setText("Waiting");
-        waiting.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(waiting);
+        iconLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(iconLabel) ;
     }
 }
